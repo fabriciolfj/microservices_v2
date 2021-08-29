f05d0a8aab588ebede6268fb7aeea31050ad35cc
fabricio <fabricio.jacob@outlook.com>
2021-08-29 14:45:09 -0300

    add

--- update-helm.sh
+++ update-helm.sh
@@ -1,3 +1,8 @@
+kubectl apply -f kubernetes/hands-on-namespace.yml
+
 for f in kubernetes/helm/components/*; do helm dep up $f; done
 for f in kubernetes/helm/environments/*; do helm dep up $f; done
 helm dep ls kubernetes/helm/environments/dev-env/
+
+
+helm install hands-on-dev-env kubernetes/helm/environments/dev-env -n hands-on --wait
